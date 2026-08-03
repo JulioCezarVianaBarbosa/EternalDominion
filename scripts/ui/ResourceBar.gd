ac71033a9af14dde198fa44e74340f772c362d42
JulioCezarVianaBarbosa <juliocezardasilvavianabarbosa@gmail.com>
2026-08-03 10:15:34 -0300

    Theme e organização

--- scripts/ui/ResourceBar.gd
+++ scripts/ui/ResourceBar.gd
@@ -1,8 +1,22 @@
-extends Control
+extends MarginContainer
 
-@export var resource_name: String = "Energia"
+@export var icon: Texture2D
+@export var value: String = "0"
+@export var max_value: String = ""
 
-@export var value: String = "120/120"
+@onready var icon_node = $Root/Content/Icon
+@onready var value_node = $Root/Content/Value
+
 
 func _ready():
-	$Root/Content/Value.text = value
+	update_bar()
+
+
+func update_bar():
+	if icon:
+		icon_node.texture = icon
+
+	if max_value.is_empty():
+		value_node.text = value
+	else:
+		value_node.text = value + "/" + max_value
